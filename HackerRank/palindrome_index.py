3eecf03225fc5473676561072976214b7e66936e
Ajsteph89 <ajsteph89@gmail.com>
2023-02-20 15:22:03 -0500

    palindrome index question w/ notes

---
+++ HackerRank/palindrome_index.py
@@ -0,0 +1,25 @@
+# determine the index of a letter in the given string that could be removed to make the string a palindrome
+
+s = "aaab"
+    # "aaab"
+
+def palindromeIndex(s):
+    # check if initial s is a palindrome
+    if s == s[::-1]:
+        return -1
+    
+    n = len(s)
+
+    # iterate through half the string 
+    for i in range(n//2):
+        # check if first and last characters are diff
+        if s[i] != s[n-1-i]:
+            # check if everything around the diff character is a palindrome
+            if s[i:n-1-i] == s[i:n-1-i][::-1]:
+                return n-1-i
+            elif s[i+1:n-i] == s[i +1:n-i][::-1]:
+                return i
+    return -1 #no other statements return a value
+
+print(palindromeIndex(s))
+
